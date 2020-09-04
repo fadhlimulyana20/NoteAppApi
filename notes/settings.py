4a8101a9cddd2bd670c3e1dc21f8982dd59ef58c
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-05 06:13:16 +0700

    Add Note Serializer and API View

--- notes/settings.py
+++ notes/settings.py
@@ -39,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #own
     'notes_data',
+    #third party
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
